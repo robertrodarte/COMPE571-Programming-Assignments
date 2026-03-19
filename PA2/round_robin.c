4beac47ff28c2467256fb1281ee8fb126c52c63a
Nick <nicholasmarkschwartz@gmail.com>
2026-03-18 18:34:04 -0700

    modified:   PA2/round_robin.c

--- PA2/round_robin.c
+++ PA2/round_robin.c
@@ -2,13 +2,21 @@
 // File:    round_robin.c
 // Author:  Nick Schwartz, Robert Rodarte
 // Date:    2026-03-09
+<<<<<<< Updated upstream
 // Brief:   Short description of this module
+=======
+// Brief:   Round-robin process scheduler with context switch timing
+>>>>>>> Stashed changes
 //------------------------------------------------------------------------------
 #define _POSIX_C_SOURCE 200112L
 
 //------------------------------------------------------------------------------
 //             __             __   ___  __
+<<<<<<< Updated upstream
 //     | |\ | /   |    |  | |  \ |__  /__
+=======
+//     | |\ | /  ` |    |  | |  \ |__  /__`
+>>>>>>> Stashed changes
 //     | | \| \__, |___ \__/ |__/ |___ .__/
 //
 //------------------------------------------------------------------------------
@@ -17,7 +25,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+<<<<<<< Updated upstream
 #include <string.h>
+=======
+>>>>>>> Stashed changes
 #include <time.h>
 #include <signal.h>
 #include <sys/time.h>
@@ -32,6 +43,7 @@
 #define WORKLOAD3 25000
 #define WORKLOAD4 10000
 
+<<<<<<< Updated upstream
 #define QUANTUM1 1000
 #define QUANTUM2 1000
 #define QUANTUM3 1000
@@ -44,24 +56,42 @@
 //      |  \ / |__) |__  |  \ |__  |__  /__`
 //      |   |  |    |___ |__/ |___ |    .__/
 //-----------------------------------------------------------------------------
+=======
+#define DEFAULT_QUANTUM 1000
+>>>>>>> Stashed changes
 
 //-----------------------------------------------------------------------------
 //                __          __        ___  __
 //     \  /  /\  |__) |  /\  |__) |    |__  /__`
 //      \/  /~~\ |  \ | /~~\ |__) |___ |___ .__/
 //-----------------------------------------------------------------------------
+<<<<<<< Updated upstream
+=======
+static struct timespec cs_start, cs_end;
+static double cs_total = 0.0;
+static int cs_count = 0;
+static int cs_running = 0;
+static double rt_total = 0.0;
+static int process_count = 0;
+>>>>>>> Stashed changes
 
 //-----------------------------------------------------------------------------
 //      __   __   __  ___  __  ___      __   ___  __
 //     |__) |__) /  \  |  /  \  |  \ / |__) |__  /__`
 //     |    |  \ \__/  |  \__/  |   |  |    |___ .__/
 //-----------------------------------------------------------------------------
+<<<<<<< Updated upstream
 void myfunction(int param);
 
 void record_switch(double elapsed_us, double quantum_us,
 				   double times[], int *count);
 
 void print_switch_stats(const double times[], int count);
+=======
+static void myfunction(int param);
+static void record_cs(void);
+static void record_rt(double time);
+>>>>>>> Stashed changes
 
 //-----------------------------------------------------------------------------
 //      __        __          __
@@ -70,11 +100,15 @@ void print_switch_stats(const double times[], int count);
 //
 //-----------------------------------------------------------------------------
 //=============================================================================
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 int main(int argc, char const *argv[])
 {
 	pid_t pid1, pid2, pid3, pid4;
 	int running1, running2, running3, running4;
+<<<<<<< Updated upstream
 
 	// --- Context switch timing state ---
 	struct timespec t_before, t_after;
@@ -88,52 +122,92 @@ int main(int argc, char const *argv[])
 
 		myfunction(WORKLOAD1);
 
+=======
+	int ret;
+	struct timespec start, end;
+	double total1, total2, total3, total4;
+	int quantum = (argc > 1) ? atoi(argv[1]) : DEFAULT_QUANTUM;
+
+	clock_gettime(CLOCK_MONOTONIC, &start);
+
+	pid1 = fork();
+	if (pid1 == 0)
+	{
+		myfunction(WORKLOAD1);
+>>>>>>> Stashed changes
 		exit(0);
 	}
 	kill(pid1, SIGSTOP);
 
 	pid2 = fork();
+<<<<<<< Updated upstream
 
 	if (pid2 == 0)
 	{
 
 		myfunction(WORKLOAD2);
 
+=======
+	if (pid2 == 0)
+	{
+		myfunction(WORKLOAD2);
+>>>>>>> Stashed changes
 		exit(0);
 	}
 	kill(pid2, SIGSTOP);
 
 	pid3 = fork();
+<<<<<<< Updated upstream
 
 	if (pid3 == 0)
 	{
 
 		myfunction(WORKLOAD3);
 
+=======
+	if (pid3 == 0)
+	{
+		myfunction(WORKLOAD3);
+>>>>>>> Stashed changes
 		exit(0);
 	}
 	kill(pid3, SIGSTOP);
 
 	pid4 = fork();
+<<<<<<< Updated upstream
 
 	if (pid4 == 0)
 	{
 
 		myfunction(WORKLOAD4);
 
+=======
+	if (pid4 == 0)
+	{
+		myfunction(WORKLOAD4);
+>>>>>>> Stashed changes
 		exit(0);
 	}
 	kill(pid4, SIGSTOP);
 
 	/************************************************************************************************
+<<<<<<< Updated upstream
 		At this point, all  newly-created child processes are stopped, and ready for scheduling.
+=======
+		At this point, all newly-created child processes are stopped, and ready for scheduling.
+>>>>>>> Stashed changes
 	*************************************************************************************************/
 
 	/************************************************************************************************
 		- Scheduling code starts here
+<<<<<<< Updated upstream
 		- Below is a sample schedule. (which scheduling algorithm is this?)
 		- For the assignment purposes, you have to replace this part with the other scheduling methods
 		to be implemented.
+=======
+		- Round-robin: each process receives one quantum per cycle, in order.
+		  Processes that have exited are skipped in subsequent cycles.
+>>>>>>> Stashed changes
 	************************************************************************************************/
 
 	running1 = 1;
@@ -145,6 +219,7 @@ int main(int argc, char const *argv[])
 	{
 		if (running1 > 0)
 		{
+<<<<<<< Updated upstream
 			clock_gettime(CLOCK_MONOTONIC, &t_before);
 			kill(pid1, SIGCONT);
 			usleep(QUANTUM1);
@@ -153,10 +228,32 @@ int main(int argc, char const *argv[])
 
 			double elapsed_us = (t_after.tv_sec - t_before.tv_sec) * 1e6 + (t_after.tv_nsec - t_before.tv_nsec) / 1e3;
 			record_switch(elapsed_us, QUANTUM1, switch_times, &switch_count);
+=======
+			record_cs();
+
+			kill(pid1, SIGCONT);
+			usleep(quantum);
+
+			kill(pid1, SIGSTOP);
+
+			clock_gettime(CLOCK_MONOTONIC, &cs_start);
+			cs_running = 1;
+
+			ret = waitpid(pid1, NULL, WNOHANG);
+			if (ret > 0)
+			{
+				clock_gettime(CLOCK_MONOTONIC, &end);
+				total1 = (end.tv_sec - start.tv_sec) + ((end.tv_nsec - start.tv_nsec) / 1E9);
+				record_rt(total1);
+				running1 = 0;
+				printf("Process 1 Time: %f\n", total1);
+			}
+>>>>>>> Stashed changes
 		}
 
 		if (running2 > 0)
 		{
+<<<<<<< Updated upstream
 			clock_gettime(CLOCK_MONOTONIC, &t_before);
 			kill(pid2, SIGCONT);
 			usleep(QUANTUM2);
@@ -192,22 +289,99 @@ int main(int argc, char const *argv[])
 		waitpid(pid2, &running2, WNOHANG);
 		waitpid(pid3, &running3, WNOHANG);
 		waitpid(pid4, &running4, WNOHANG);
+=======
+			record_cs();
+
+			kill(pid2, SIGCONT);
+			usleep(quantum);
+			kill(pid2, SIGSTOP);
+
+			clock_gettime(CLOCK_MONOTONIC, &cs_start);
+			cs_running = 1;
+
+			ret = waitpid(pid2, NULL, WNOHANG);
+			if (ret > 0)
+			{
+				clock_gettime(CLOCK_MONOTONIC, &end);
+				total2 = (end.tv_sec - start.tv_sec) + ((end.tv_nsec - start.tv_nsec) / 1E9);
+				record_rt(total2);
+				running2 = 0;
+				printf("Process 2 Time: %f\n", total2);
+			}
+		}
+
+		if (running3 > 0)
+		{
+			record_cs();
+
+			kill(pid3, SIGCONT);
+			usleep(quantum);
+			kill(pid3, SIGSTOP);
+
+			clock_gettime(CLOCK_MONOTONIC, &cs_start);
+			cs_running = 1;
+
+			ret = waitpid(pid3, NULL, WNOHANG);
+			if (ret > 0)
+			{
+				clock_gettime(CLOCK_MONOTONIC, &end);
+				total3 = (end.tv_sec - start.tv_sec) + ((end.tv_nsec - start.tv_nsec) / 1E9);
+				record_rt(total3);
+				running3 = 0;
+				printf("Process 3 Time: %f\n", total3);
+			}
+		}
+
+		if (running4 > 0)
+		{
+			record_cs();
+
+			kill(pid4, SIGCONT);
+			usleep(quantum);
+			kill(pid4, SIGSTOP);
+
+			clock_gettime(CLOCK_MONOTONIC, &cs_start);
+			cs_running = 1;
+
+			ret = waitpid(pid4, NULL, WNOHANG);
+			if (ret > 0)
+			{
+				clock_gettime(CLOCK_MONOTONIC, &end);
+				total4 = (end.tv_sec - start.tv_sec) + ((end.tv_nsec - start.tv_nsec) / 1E9);
+				record_rt(total4);
+				running4 = 0;
+				printf("Process 4 Time: %f\n", total4);
+			}
+		}
+>>>>>>> Stashed changes
 	}
 
 	/************************************************************************************************
 		- Scheduling code ends here
 	************************************************************************************************/
 
+<<<<<<< Updated upstream
 	print_switch_stats(switch_times, switch_count);
 
 	return 0;
 }
+=======
+	printf("Average Response Time: %f s\n", rt_total / process_count);
+	printf("Context Switch Count: %d\n", cs_count);
+	printf("Total Context Switch Time: %.3f ns\n", cs_total * 1E9);
+	printf("Avg Context Switch Time: %.3f ns\n", (cs_total / cs_count) * 1E9);
+
+	return 0;
+}
+
+>>>>>>> Stashed changes
 //-----------------------------------------------------------------------------
 //      __   __              ___  ___
 //     |__) |__) | \  /  /\   |  |__
 //     |    |  \ |  \/  /~~\  |  |___
 //
 //-----------------------------------------------------------------------------
+<<<<<<< Updated upstream
 void record_switch(double elapsed_us, double quantum_us,
 				   double times[], int *count)
 {
@@ -251,6 +425,33 @@ void print_switch_stats(const double times[], int count)
 void myfunction(int param)
 {
 
+=======
+//=============================================================================
+static void record_cs(void)
+{
+	if (cs_running)
+	{
+		clock_gettime(CLOCK_MONOTONIC, &cs_end);
+		cs_total += (cs_end.tv_sec - cs_start.tv_sec) + ((cs_end.tv_nsec - cs_start.tv_nsec) / 1E9);
+		cs_count++;
+		cs_running = 0;
+	}
+}
+
+//=============================================================================
+static void record_rt(double time)
+{
+	rt_total += time;
+	process_count++;
+}
+
+//=============================================================================
+/************************************************************************************************
+					DO NOT CHANGE THE FUNCTION IMPLEMENTATION
+*************************************************************************************************/
+static void myfunction(int param)
+{
+>>>>>>> Stashed changes
 	int i = 2;
 	int j, k;
 
@@ -278,9 +479,13 @@ void myfunction(int param)
 //        __   __   __
 //     | /__ |__) /__
 //     | .__/ |  \ .__/
+<<<<<<< Updated upstream
 //-----------------------------------------------------------------------------
 
 /************************************************************************************************
 		These DEFINE statements represent the workload size of each task and
 		the time quantum values for Round Robin scheduling for each task.
 *************************************************************************************************/
+=======
+//-----------------------------------------------------------------------------a
+>>>>>>> Stashed changes
